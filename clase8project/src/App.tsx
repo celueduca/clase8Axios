91745d8e4f5d1af6d0cc45825de68831bd028afb
YefersonZuluaga <ahumada1248@gmail.com>
2021-11-26 19:06:54 -0500

    correcion componente

--- clase8project/src/App.tsx
+++ clase8project/src/App.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import HttpClient from './helpers/HttpClient'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import PageLocation from './Pages/PageLocation';
-import PageCharacter from './Pages/PageCharacter';
-import PageMain from './Pages/PageMain';
-import PrivateRoute from'./Pages/PrivateRoute';
 
 interface Character {
   created: string,
@@ -12,52 +7,43 @@ interface Character {
   image: string
 }
 
-interface Location{
-  name:string;
-  type:string;
-  image:string;
-}
-
 function App() {
 
   const [characters, setCharacters] = useState([]);
-  const [locations , setLocations] = useState([]);
 
   const loadCharactersRM = async () => {
 
-    await HttpClient.get('/character/?page=4')
+    await HttpClient.get('/character/?page=2')
       .then((res) => {
-       setCharacters(res.data.results);
+        setCharacters(res.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
-  const loadLocationsRM = async () => {
-
-    await HttpClient.get('/location/?page=4')
-      .then((res) => {
-        setLocations(res.data.results)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  useEffect(() => {
+    loadCharactersRM();
+  }, [])
 
   return (
-
     <>
-    <BrowserRouter>
-      <Switch>
-        
-        <Route exact path="/pageLocation" component={PageLocation}/>
-        <Route exact path="/pageCharacter" component={PageCharacter}/>
-        <Route exact path="/" component={PageMain}/>
-      </Switch>
-    </BrowserRouter>
+      {
+        characters.length > 0 &&
+        <>
+          {characters.map((character: Character, j) => (
+            <>
+              <h2>{character.name}</h2>
+              <h3>{character.created}</h3>
+              <img src={character.image} />
+            </>
+          ))}
+
+          <button>Atrás</button>
+          <button>Siguiente</button>
+        </>
+      }
     </>
-   
   );
 }
 
